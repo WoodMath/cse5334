3bd51dfffa03e2f5d4cd6693307799f4b4e326fa
WoodMath <jeffwood314@gmail.com>
2016-02-18 22:34:44 -0600

    Updated 'fnSortedIndex' to take index and return index of sorted elements, this time accounting for non-unique elements

---
+++ hw01/fnKNN.m
@@ -0,0 +1,7 @@
+function [ output_args ] = fnKNN( mat_test, mat_train, mat_class, i_k)
+%UNTITLED5 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
--- hw01/fnPointNearest.m
+++ hw01/fnPointNearest.m
@@ -1,31 +1,32 @@
-% function [] = fnDist(v_point, v_group)
+% function [] = fnPointsNearest(mat_point, mat_group, mat_class, k)
 % fnDist 
-%   measures distance betwen EACH 'test point' in 'v_point' and 
-%   EVERY 'group point' in 'v_group'
+%   measures distance betwen EACH 'test point' in 'mat_point' and 
+%   EVERY 'group point' in 'mat_group'
     
-    v_point=[2,3;4,5;7,8];
-    v_group=[1,3;3,6;7,8;9,10;1,5];
+    k=3;
+
+    mat_point = [1,12;3,9;9,5;11,5];
+    mat_group = [2,10;2,8;3,7;4,10;4,8;5,9;7,5;8,6;9,7;10,6;10,4;8,4];
+    mat_class = [1,1,1,1,1,1,2,2,2,2,2,2];
+    
     
     %% Rearrange Test points along 3rd axis
-    v_point = permute(v_point, [3,2,1]);
+    mat_point = permute(mat_point, [3,2,1]);
     % Replicate for size of points to whose distance you wish to calculate
     % (For each test point)
-    v_points = repmat(v_point, [size(v_group,1),1,1]);
-     
-    v_group = repmat(v_group, [1,1,size(v_points,3)]); 
-    v_diff = v_points-v_group;
-    v_dist = v_diff.^2;
-    v_dist = sum(v_dist,2);
-    v_dist = permute(v_dist,[1,3,2]);
+    mat_points = repmat(mat_point, [size(mat_group,1),1,1]);
+    
+    %% Get toal distance
+    mat_group = repmat(mat_group, [1,1,size(mat_points,3)]); 
+    mat_diff = mat_points-mat_group;
+    mat_dist = mat_diff.^2;
+    
+    %% Sum along all dimensions
+    mat_dist = sum(mat_dist,2);
+    mat_dist = permute(mat_dist,[1,3,2]);
 
-    v_index = fnSortedIndex(v_dist);
+    mat_index = fnSortedIndex(mat_dist);
     %% Distances between each Test points occur along 2nd 
-%     v_dist_rearranged = permute(v_dist, [3, 2, 1]);
-%     v_dist_rearranged = repmat(v_dist_rearranged, [size(v_dist_rearranged,3),1,1])
-%     %   find ordered index
-%     v_sorted = sort(v_dist);
-%     v_sorted_rearranged = repmat(v_sorted, [1,1,size(v_sorted,1)])
-% 
-%     v_equal = v_dist_rearranged == v_sorted_rearranged
-%     error('message');
+
+    
 % end
