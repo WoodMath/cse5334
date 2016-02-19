f63aa9b667965f66032eed1cd58777359aeba17e
WoodMath <jeffwood314@gmail.com>
2016-02-19 00:46:43 -0600

    Time to go

--- hw01/fnPointNearest.m
+++
@@ -1,8 +0,0 @@
-% function [] = fnPointsNearest(mat_point, mat_group, mat_class, k)
-% fnDist 
-%   measures distance betwen EACH 'test point' in 'mat_point' and 
-%   EVERY 'group point' in 'mat_group'
-    
-
-
-% end
