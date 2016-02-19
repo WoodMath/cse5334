6268bd824d6e4879412f9c49e38f13b99bcc8baf
WoodMath <jeffwood314@gmail.com>
2016-02-19 16:45:26 -0600

    Changes to 'centroid' code

--- hw01/fnKNN.m
+++ hw01/fnKNN.m
@@ -2,33 +2,39 @@ function [ v_class, mat_classes_of_closest ] = fnKNN( mat_test, mat_train, mat_c
 %fnKNN Summary of this function goes here
 %   Detailed explanation goes here
 
-%     mat_test = [1,11;11,3;6,8;6,7;6,6];
+    i_k=3;
+    mat_test = [1,11;11,3;6,8;6,7;6,6];
 % %     mat_point = [6,6];
-%     mat_train = [2,10;2,8;3,7;4,10;4,8;5,9;7,5;8,6;9,7;10,6;10,4;8,4];
-%     mat_class = [1,1,1,1,1,1,2,2,2,2,2,2]';
+    mat_train = [2,10;2,8;3,7;4,10;4,8;5,9;7,5;8,6;9,7;10,6;10,4;8,4];
+    mat_class = [1,1,1,1,1,1,2,2,2,2,2,2]';
 %     mat_class = mat_class*2;
+
+    mat_temp = fnDist(mat_test, mat_train);
+    mat_dist = mat_temp.^2;
 
     if(mod(i_k,2)==0)
         error(['Error : value of k = ', int2str(i_k), ' is not an odd value.'])
     end
 
 
-    %% Rearrange Test points along 3rd axis
-    mat_test = permute(mat_test, [3,2,1]);
-    % Replicate for size of points to whose distance you wish to calculate
-    % (For each test point)
-    mat_points = repmat(mat_test, [size(mat_train,1),1,1]);
-    
-    %% Get toal distance
-    mat_train = repmat(mat_train, [1,1,size(mat_points,3)]); 
-    mat_diff = mat_points-mat_train;
-    mat_dist = mat_diff.^2;
-    
-    %% Sum along all dimensions of each point
-    mat_dist = sum(mat_dist,2);
-    mat_dist = permute(mat_dist,[1,3,2]);
+%     %% Rearrange Test points along 3rd axis
+%     mat_test = permute(mat_test, [3,2,1]);
+%     % Replicate for size of points to whose distance you wish to calculate
+%     % (For each test point)
+%     mat_points = repmat(mat_test, [size(mat_train,1),1,1]);
+%     
+%     %% Get toal distance
+%     mat_train = repmat(mat_train, [1,1,size(mat_points,3)]); 
+%     mat_diff = mat_points-mat_train;
+%     mat_dist = mat_diff.^2;
+%     
+%     %% Sum along all dimensions of each point
+%     mat_dist = sum(mat_dist,2);
+%     mat_dist = permute(mat_dist,[1,3,2]);
 
     i_num_test_points = size(mat_dist,2);
+    
+    mat_class=reshape(mat_class, [length(mat_class),1]);
     
     mat_index = fnSortedIndex(mat_dist);
 
