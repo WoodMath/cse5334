4fbe27467d08330330f839adbb5648ac86498aa4
JeffWood <jeffwood314@gmail.com>
2016-03-31 18:02:11 -0500

    Finalized project demo re-do

--- hw02/fnConfusion.m
+++ hw02/fnConfusion.m
@@ -22,39 +22,18 @@ function [ mat_confusion, mat_correct, i_correct, f_correct ] = fnConfusion( mat
         end
     end
 
-    %% Get permutations for correct ordering of confusion matrix
-    [v_permute] = maxWeightMatching(mat_edges);
-    
-    %% Strip off first part of the array ang get mapping
-    v_permute = v_permute(1:i_k)-i_k;
-    
-    %% Some will be valid mappings
-    v_valid_mappings_ind = v_permute>=0;
-    v_invalid_mappings_ind = not(v_valid_mappings_ind);
-    
-    v_valid_mappings = v_valid_mappings_ind.*(v_permute);
-    v_valid_mappings_sorted = sortrows(horzcat(v_valid_mappings',[1:i_k]'));
-    
-    mat_ind = repmat((1:i_k)',1,i_k);
-    mat_map = repmat(v_valid_mappings,i_k,1);
-    
-    %% Get indices of columns not mapped (any number that does not appear)
-    v_invalid_mappings = not(sum(mat_ind==mat_map,2)').*[1:i_k];
-    v_invalid_mappings = sort(v_invalid_mappings')';
-    v_invalid_mappings = v_invalid_mappings(v_invalid_mappings>0);
-    
-    %% Put indices of columns not mapped backinto 0s spot
-    v_valid_mappings_sorted(v_valid_mappings_sorted==0,1)=v_invalid_mappings';
-    v_valid_mappings_unsorted = [v_valid_mappings_sorted(:,2), v_valid_mappings_sorted(:,1)];
-    v_permutations = sortrows(v_valid_mappings_unsorted)';
-    v_new_permute = v_permutations(2,:);
-    
-    mat_correct = mat_confusion(:,v_new_permute);
-    
+    %% Hungarian Alorithm Minimizes cost but we need to maximize cost so transform to
+    mat_use = max(max(mat_confusion)) - mat_confusion;
+    v_permute = hungarian(mat_use);
     
     %% Calculate percentage correct
+    mat_correct = mat_confusion(v_permute,:); 
+    
+    
     i_correct = trace(mat_correct);
     f_correct = i_correct/size(mat_train,1);
+    return
+    
     
 end
 
